8688305b2b82c073d8bd48fd497aac8c33661f55
김민주 <minju@gimminjuui-MacBookAir.local>
2024-10-11 11:31:23 +0900

    [section12]

---
+++ react_basic/section12/src/App.jsx
@@ -0,0 +1,59 @@
+import './App.css';
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Diary from './pages/Diary';
+import New from './pages/New';
+import Notfound from './pages/Notfound';
+import Button from './components/Button';
+
+import { getEmotionImage } from './util/get-emotion-image';
+
+// 1. "/": 모든 일기를 조회하는 Home 페이지
+// 2. "/new": 새로운 일기를 작성하는 new 페이지
+// 3. "/diary": 일기를 상세히 조회하는 Diary 페이지
+
+function App() {
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("/new");
+  };
+
+  return (
+  <>
+  <Button 
+    text={"123"}
+    type={"DEFAULT"}
+    onClick={() => {
+      console.log("123번 버튼 클릭!")
+    }} 
+  />
+
+  <Button 
+    text={"123"}
+    type={"POSITIVE"}
+    onClick={() => {
+      console.log("123번 버튼 클릭!")
+    }} 
+  />
+
+  <Button 
+    text={"123"}
+    type={"NEGATIVE"}
+    onClick={() => {
+      console.log("123번 버튼 클릭!")
+    }} 
+  />
+  
+  <Routes>
+    <Route path="/" element={<Home />}/>
+    <Route path="/new" element={<New />}/>
+    <Route path="/diary/:id" element={<Diary />}/>
+    <Route path="*" element={<Notfound />}/>
+  </Routes>
+  </>
+  );
+};
+
+
+export default App
